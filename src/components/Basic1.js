e8c26fee2d926e8cd3fed927f837d5eb6f11c615
yugo_i <u15jpn@gmail.com>
2020-09-11 21:26:28 +0900

    countRender

---
+++ src/components/Basic1.js
@@ -0,0 +1,21 @@
+import React ,{useState} from 'react'
+
+const Basic1 = (props) => {
+
+  const clickHandler = () => {
+    console.log("clicked")
+  }
+
+  const [count, setCount] = useState(0)
+  
+  return (
+    <>
+      <button onClick={()=>{setCount(prevCount => prevCount +1); setCount(prevCount => prevCount +1);} }>Click {count}</button>
+      <h1>Hello,React{props.name}</h1>
+      <h1>Hello,React2</h1>
+    </>
+  )
+
+}
+
+export default Basic1
