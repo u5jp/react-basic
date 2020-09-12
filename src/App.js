49ce45a81862646c35586e9cdf783bf6c3165c27
yugo_i <u15jpn@gmail.com>
2020-09-12 18:43:00 +0900

    make Timer of useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Basic1 from './components/Basic1'
-import Basic2 from './components/Basic2'
+// import Basic1 from './components/Basic1'
+// import Basic2 from './components/Basic2'
+// import BasicUseEffect from './components/BasicUseEffect'
+import TimerContainer from './components/TimerContainer'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />        
-        <Basic1 />
-        <Basic2 />
+        {/* <Basic1 /> */}
+        {/* <Basic2 /> */}
+        {/* <BasicUseEffect /> */}
+        <TimerContainer />
       </header>
     </div>
   );
---
+++ src/components/BasicUseEffect.js
@@ -0,0 +1,20 @@
+import React,{useState,useEffect} from 'react'
+
+const BasicUseEffect = () => {
+
+  const [count,setCount] = useState(0)
+  const [item,setItem] = useState('')
+
+  useEffect(()=>{
+    console.log("useEffect invoked")
+  },[count])
+
+  return (
+    <div>
+      <button onClick={()=>setCount(prevCount=>prevCount+1)}>Click {count}</button>
+      <input type="text" value={item} onChange={e=>setItem(e.target.value)}/>
+    </div>
+  )
+}
+
+export default BasicUseEffect
