a694a520eecb5f0b30d8d9b5d64297655867907f
yugo_i <u15jpn@gmail.com>
2020-09-12 19:38:41 +0900

    api accsess when the button is clicked

--- src/components/ApiFetch.js
+++ src/components/ApiFetch.js
@@ -4,22 +4,34 @@ import axios from 'axios'
 const ApiFetch = () => {
 
   const [posts,setPosts] = useState([])
+  const [id, setId] = useState(1)
+  const [clicked,setClicked] = useState(false)
+
+  const handlerClicked = () => {
+    setClicked(!clicked)
+  }
 
   useEffect(()=>{
     // axios.get('https://jsonplaceholder.typicode.com/posts')
     // .then(res =>{
     //   setPosts(res.data)
     // })
-    fetch('https://jsonplaceholder.typicode.com/posts',{method:'GET'})
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{method:'GET'})
     .then(res => res.json())
     .then(data => {
       setPosts(data)
     })
-  },[])
+  },[clicked])
+
 
   return (
     <div>
-      <ul>
+      <input type="text" vlaue={id} onChange={e=>setId(e.target.value)}/>
+      <br />
+      <button type='button' onClick={handlerClicked}>Get post</button>
+      <br />
+      {posts.title}
+      {/* <ul>
         {
           posts.map(post => (
             <li key={post.id}>
@@ -27,7 +39,7 @@ const ApiFetch = () => {
             </li>
           ))
         }
-      </ul>
+      </ul> */}
     </div>
   )
 }
